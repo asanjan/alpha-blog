69c8e4a3a52cb2a01880f3323d47d806ce2d5b29
adam.s.janech <adam.s.janech@gmail.com>
2018-05-02 00:38:11 -0400

    Add edit and show actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,15 @@
-class ArticlesController < ApplicationController
+ class ArticlesController < ApplicationController
   
   def new
-    @article=Article.new
-    
+    @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def index
+    @articles = Article.all
   end
   
   def create
@@ -16,6 +23,16 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was succesfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
@@ -24,4 +41,6 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :description )
   end
+  
+  
 end
